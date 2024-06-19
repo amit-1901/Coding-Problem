2a43c4c09d6c427d9b13d64f5be94212b5877994
AMIT AGRAHARI <119153920+amit-1901@users.noreply.github.com>
2024-06-19 18:27:52 +0530

    Basic String Method
    string methods

---
+++ string.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+ string str1="amit agrahari",str2;
+ cout<<"the string is : "<<str1<<endl;
+ cout<<"Enter the string: ";
+ //cin>>str2;
+ getline(cin,str2);
+ cout<<"the string is : "<<str2<<endl;
+ //string str=str1 + str2;
+ //cout<<"the string is : "<<str1+str2;
+// string str=str1.append(str2);
+// cout<<"the string is : "<<str;
+//cout<<str1.length(); 
+//cout<<str2.size(); 
+//cout<<str1[2];
+//str1[10]='z'
+//cout<<str1;
+return 0;
+}
+

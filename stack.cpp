23cd4b5a331df1ecb504eb91ab8df9d75ab9bfa8
AMIT AGRAHARI <119153920+amit-1901@users.noreply.github.com>
2024-07-04 17:06:42 +0530

    all about stack

---
+++ stack.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    cout<<"Hello World!"<<endl;
+    stack<int> s;
+    s.push(1);
+    s.push(2);
+    s.push(3);
+    cout<<s.top()<<endl;
+    s.pop();
+    cout<<s.top();
+    //if(s.empty())
+    return 0;
+}
+
